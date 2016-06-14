33548f2593a00115f17c6d57500d981e7637f54e
ishdaddy7 <seanhan1@gmail.com>
2016-06-14 13:00:32 -0400

    got through initial GET

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // Main fiel
+var express = require( 'express' );
+var app = express(); // creates an instance of an express application
+
+app.use(function(req, res, next){
+  res.send('it worked!');
+});
+
+app.get('/', function(req, res, next){
+  console.log('it worked!');
+  res.send('it worked!');
+});
+
+app.listen(3000);
