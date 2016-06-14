e6072902ebd36055c2abb602430e16e31a89bedc
ishdaddy7 <seanhan1@gmail.com>
2016-06-14 13:15:28 -0400

    through 'starting a server'

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var express = require( 'express' );
 var app = express(); // creates an instance of an express application
 
 app.use(function(req, res, next){
-  res.send('it worked!');
+  next();
+});
+
+app.get('/news', function(req, res, next){
+  console.log('logging the news');
+  res.send('sending the news');
 });
 
 app.get('/', function(req, res, next){
   console.log('it worked!');
   res.send('it worked!');
 });
+
 
 app.listen(3000);
