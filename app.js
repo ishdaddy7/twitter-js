d90dad5e645bfb1957db41d22d7b0bb9c8d10036
Jackson Brietzke <jack.brietzk@gmail.com>
2016-06-14 14:28:46 -0400

    Finished Configuring Express

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var express = require( 'express' );
 var app = express(); // creates an instance of an express application
 
 app.use(function(req, res, next){
+  console.log("Request type: ", req.method);
+  console.log("The route: ", req.path);
+  next();
+});
+
+app.use("/special",function(req, res, next){
+  console.log("Request type #2: ", req.method);
+  console.log("The route #2: ", req.path);
   next();
 });
 
