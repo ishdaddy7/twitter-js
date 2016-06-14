0a5afd4355e5f00f47158115942606c36961a054
ishdaddy7 <seanhan1@gmail.com>
2016-06-14 15:46:31 -0400

    got template testing to work

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 var express = require( 'express' );
 var swig = require("swig");
 var app = express(); // creates an instance of an express application
+swig.setDefaults({ cache: false }); // added this for dev, since we'll be changing the file a lot. bad for production.
 
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 
 app.use(function(req, res, next){
@@ -24,8 +28,13 @@ app.get('/news', function(req, res, next){
 
 app.get('/', function(req, res, next){
   console.log('it worked!');
-  res.send('it worked!');
+  var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  var obj = [{name: 'Fullstack'}, {name: 'App Academy'}];
+  res.render( 'index', {title: 'Hall of Fame', people: people, sean: 'Hi Sean', jack: 'Hey Jack', obj: obj} );
 });
 
+app.use(function(err, req, res, next){
+  console.log(err);
+});
 
 app.listen(3000);
